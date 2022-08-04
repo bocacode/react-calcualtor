5eba3c2c98e1b737efe9c692426448178b827e6c
Damian Montero <photoca@hotmail.com>
2022-08-04 17:12:44 -0400

    adding inline and object css

--- src/App.js
+++ src/App.js
@@ -3,22 +3,23 @@ import './App.css';
 
 function App() {
   const [total,setTotal] = useState(0)
-
+  
   let digits = [1,2,3,4,5,6,7,8,9,0]
 
   return (
     <div className="App">
       <header className="App-header">
-        <div>Total: {total} </div>
+        <div style={styles.title}>Total: {total} </div>
+          <div id="calculator">
 
           {digits.map((digit)=>{
-            return <button key={digit} onClick={()=>{
+            return <button style={{ color: "black", backgroundColor: "darksalmon" }} key={digit} onClick={()=>{
                setTotal(total + digit)
               }
             }>{digit}</button>
           })}
         <button onClick={()=>{setTotal(0)}}>reset</button>
-        
+        </div>
         {/* 
         <button onClick={()=>{setTotal(total + 1)}}>1</button>
         <button onClick={()=>{setTotal(total + 2)}}>2</button>
@@ -41,5 +42,19 @@ function App() {
     </div>
   );
 }
+
+
+const styles = {
+  title: {
+    fontSize: 36,
+    fontWeight: '900',
+    color: 'lemonchiffon'
+  },
+  screen: {
+    backgroundColor: 'black',
+    padding: 16
+  }
+}
+
 
 export default App;
